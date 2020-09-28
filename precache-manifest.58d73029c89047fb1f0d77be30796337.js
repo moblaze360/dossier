0b265c5d87d75a071a640d7dd12e01db464b8fc7
moblaze360 <victorvalenti@yahoo.co.uk>
2020-09-28 23:12:36 +0200

    Add files via upload

---
+++ precache-manifest.58d73029c89047fb1f0d77be30796337.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f6ed172716a0a50ce5e49b09e3f180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82144ecc970dc7f09142",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "54f9170e2979573c6059",
+    "url": "/static/css/main.89b61ab7.chunk.css"
+  },
+  {
+    "revision": "82144ecc970dc7f09142",
+    "url": "/static/js/2.d398c6c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d398c6c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f9170e2979573c6059",
+    "url": "/static/js/main.51402130.chunk.js"
+  },
+  {
+    "revision": "0b05826a0e2dd872ad25",
+    "url": "/static/js/runtime-main.336fc262.js"
+  },
+  {
+    "revision": "814db3fcbb4a092cf9ea63fabe00f8ce",
+    "url": "/static/media/NewSuperman-Pzd7.814db3fc.woff"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0172c651ecde833ba460713268daeeae",
+    "url": "/static/media/batmfa__.0172c651.woff"
+  },
+  {
+    "revision": "e6e10fdf66cf9f4f84d2b269586cf040",
+    "url": "/static/media/batmfo__.e6e10fdf.woff"
+  }
+]);
